bf8ccef1b98857a13c9736365caf24b8cf3a04d3
abutler911 <abutler911@gmail.com>
2022-08-10 22:48:32 -0600

    adding post route and axios

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const express = require("express");
-// const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 //static files
 
 app.use(express.static(__dirname + "/public"));
@@ -15,6 +18,18 @@ app.set("view engine", "ejs");
 //Routes
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=${req.body.city}`
+    );
+    const weatherData = response.data;
+    console.log(weatherData);
+  } catch (error) {
+    console.log(error.response.body);
+  }
 });
 
 app.listen(port, () => {
