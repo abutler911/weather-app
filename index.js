08f08523a3801b461bc0f147d8facdb40df776fc
abutler911 <abutler911@gmail.com>
2022-08-11 19:34:51 -0600

    MVP

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const { query } = require("express");
 
 const app = express();
 const port = 3000;
@@ -20,13 +21,33 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/result", (req, res) => {
+  res.render("result");
+});
+
 app.post("/", async (req, res) => {
   try {
     const response = await axios.get(
       `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=${req.body.city}`
     );
     const weatherData = response.data;
-    console.log(weatherData);
+    const location = response.data.request.query;
+    const time = response.data.current.observation_time;
+    const windDirection = response.data.current.wind_dir;
+    const windSpeed = response.data.current.wind_speed;
+    const temp = response.data.current.temperature;
+    const iconURL = response.data.current.weather_icons;
+
+    const result = {
+      location: location,
+      time: time,
+      windDirection: windDirection,
+      windSpeed: windSpeed,
+      temp: temp,
+      icon: iconURL,
+    };
+    console.log(result);
+    res.render("home", { result: result });
   } catch (error) {
     console.log(error.response.body);
   }
